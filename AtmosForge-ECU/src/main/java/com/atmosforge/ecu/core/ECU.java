135d980befe65a31ff0670c2c2cad37c796dbf86
henryhazelton-uni <30250725@blackpool.ac.uk>
2025-10-17 11:38:01 +0100

    updated core

--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/core/ECU.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/core/ECU.java
@@ -1,9 +1,14 @@
-import Sensors.TemperatureSensor;
-import Sensors.PressureSensor;
-import Sensors.HumiditySensor;
-import Controllers.TemperatureController;
-import Controllers.PressureController;
-import Controllers.HumidityController;
+package com.atmosforge.ecu.core;
+
+// Sensors
+import com.atmosforge.ecu.sensors.TemperatureSensor;
+import com.atmosforge.ecu.sensors.PressureSensor;
+import com.atmosforge.ecu.sensors.HumiditySensor;
+
+// Controllers
+import com.atmosforge.ecu.controllers.TemperatureController;
+import com.atmosforge.ecu.controllers.PressureController;
+import com.atmosforge.ecu.controllers.HumidityController;
 
 public class ECU {
     public static void main(String[] args) {
