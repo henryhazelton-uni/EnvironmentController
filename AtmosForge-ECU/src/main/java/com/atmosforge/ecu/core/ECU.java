3a9097d186fb7169578c11f77a7a33f70488b037
henryhazelton-uni <30250725@blackpool.ac.uk>
2025-10-22 09:33:49 +0100

    updated pom.xml file

--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/core/ECU.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/core/ECU.java
@@ -1,11 +1,11 @@
 package com.atmosforge.ecu.core;
 
 public class ECU {
-    public class Main {
+    
     public static void main(String[] args) {
         System.out.println("Starting AtmosForge Environment Control Unit...");
         // Later: initialize your ECU modules or JavaFX app here
-    }
+    
 }
     
 }
