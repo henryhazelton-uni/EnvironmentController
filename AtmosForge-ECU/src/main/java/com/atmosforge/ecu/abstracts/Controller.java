4d35f376cd22d10052047d49b012e4f15e7aa051
henryhazelton-uni <30250725@blackpool.ac.uk>
2025-10-22 09:20:52 +0100

    added ds_store to gitignore

--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/abstracts/Controller.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/abstracts/Controller.java
@@ -3,5 +3,17 @@ package com.atmosforge.ecu.abstracts;
 import com.atmosforge.ecu.interfaces.ControllerInterface;
 
 public class Controller implements ControllerInterface{
+
+    @Override
+    public void checkSensor() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSensor'");
+    }
+
+    @Override
+    public void alter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alter'");
+    }
     
 }
