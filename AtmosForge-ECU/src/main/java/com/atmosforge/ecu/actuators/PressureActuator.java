ce1cb701d27fe3c7f68e02b2d638d4b526e237dd
henryhazelton-uni <30250725@blackpool.ac.uk>
2025-11-05 10:48:33 +0000

    initalised a logger to leaf classes

--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/actuators/PressureActuator.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/actuators/PressureActuator.java
@@ -1,9 +1,15 @@
 package com.atmosforge.ecu.actuators;
 
 import com.atmosforge.ecu.abstracts.Actuator;
+import com.atmosforge.ecu.core.LoggingManager;
+import com.atmosforge.ecu.interfaces.LoggerInterface;
 
 public class PressureActuator extends Actuator
 {
+
+    // Initalise logger for pressure actuator
+    private static final LoggerInterface logger = LoggingManager.getLogger();
+
     public PressureActuator()
     {
         
