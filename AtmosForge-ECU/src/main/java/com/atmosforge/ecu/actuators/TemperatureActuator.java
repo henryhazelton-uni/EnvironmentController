72f84e003b69d08d9b75ef21ea81437b548004fc
henryhazelton-uni <30250725@blackpool.ac.uk>
2025-11-05 15:25:55 +0000

    tidied up

--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/actuators/TemperatureActuator.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/actuators/TemperatureActuator.java
@@ -4,8 +4,10 @@ import com.atmosforge.ecu.abstracts.Actuator;
 
 public class TemperatureActuator extends Actuator
 {
+
     public TemperatureActuator(String name)
     {
         super(name);
     }
+    
 }
