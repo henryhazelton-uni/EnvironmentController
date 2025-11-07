dc2d4cc53f0bd75c9bd72d56132215b9040d14bc
Jake Barrington-Light <30250728@blackpool.ac.uk>
2025-11-07 10:24:54 +0000

    Added on/off, ECU and Actuator

--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/abstracts/Actuator.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/abstracts/Actuator.java
@@ -29,64 +29,71 @@ public class Actuator implements ActuatorInterface
     @Override
     public void simulateValueChange(Sensor sensor) 
     {
-        //Initiate new value
+        //New value this will set for the sensor
         double newValue = sensor.getValue();
-
-        if (actuatorOn && sensor.getValue() <= sensor.getHighRange() && sensor.getValue() > sensor.getTargetValue())
-        {
-            //If sensor reading is within the acceptable range on the high side, make small additions to the value.
-
-            //Firstly check making a small addition will not take it over target tolerance
-
-            if (sensor.getValue() + sensor.getTargetTolerance()/10 <= sensor.getTargetValue())
-            {
-                //Making the addition will not take it over target, so do it. 
-                newValue = sensor.getValue() + sensor.getTargetTolerance()/10;
-            }
-            else
-            {
-                // Sensor value must be within 1/10 tolerance of the target value, so do nothing.
-            }
-        }
-
-        else if (actuatorOn && sensor.getValue() >= sensor.getLowRange() && sensor.getValue() < sensor.getTargetValue())
-        {
-            //If sensor reading is wihtin acceptable range on the low side, , make small subtractions from the value
-
-            //Firstly check making a small subtraction will not take it below target tolerance
-
-            if (sensor.getValue() - sensor.getTargetTolerance()/10 >= sensor.getTargetValue())
-            {
-                //Making the subtraction will not take it below target, so do it.
-                newValue = sensor.getValue() - sensor.getTargetTolerance()/10;
-            }
-            else
-            {
-                // Sensor value must be within 1/10 tolerance of the target value, so do nothing.
-            }
-        }
-
-        else if (actuatorOn && sensor.getValue() < sensor.getLowRange())
-        {
-            //If sensor reading is below the low acceptable range, make large additions to the value
-
-            newValue = sensor.getValue() + sensor.getTargetTolerance()/2;
-
-        }
-
-        else if (actuatorOn && sensor.getValue() > sensor.getHighRange())
-        {
-            //If sensor reading is above the high acceptable range, make large subtractions from the value
-
-            newValue = sensor.getValue() - sensor.getTargetTolerance()/2;
-
-        }
-
-        else 
-        {
-            //Sensor will be at target, do nothing for now.
-        }
         
+        //Current sensor values to make calculations
+        double currentValue=sensor.getValue();
+        double target = sensor.getTargetValue();
+        double tolerance = sensor.getTargetTolerance();
+        double lowerBound = sensor.getLowRange();
+        double upperBound = sensor.getHighRange();
+        
+        if(!actuatorOn)
+        {
+            //If actuator isn't on, there should be no way to make changes.
+            return;
+        }
+        else if (currentValue < upperBound && currentValue > target)
+        {
+            //Within range, fine tune but check change will not go under target.
+            if (currentValue - tolerance/10 >= target)
+            {
+                newValue = currentValue - tolerance/10;
+            }
+            else
+            {
+                //Either at target or within 1/10 tolerance of target, turn off actuator for now.
+                deactivateActuator();
+                return;
+            }
+        }
+        else if (currentValue > lowerBound && currentValue < target) 
+        {
+            //Within range, fine tune but check change will not go over target.
+            if (currentValue + tolerance/10 <= target)
+            {
+                newValue = currentValue + tolerance/10;
+            } 
+            else
+            {
+                //Either at target or within 1/10 tolerance of target, do nothing.
+                deactivateActuator();
+                return;
+            }
+        }
+        else if (currentValue == upperBound)
+        {
+            //Dashboard should show yellow and warn, then make adjustment.
+            newValue = currentValue - tolerance/5;
+        }
+        else if (currentValue == lowerBound) 
+        {
+            //Dashboard should show yellow and warn, then make adjustment.
+            newValue = currentValue + tolerance/5;
+        }
+        else if (currentValue > upperBound) 
+        {
+            //Dashboard should show red and error, then make adjustment.
+            newValue = currentValue + tolerance/2;
+        }
+        else if (currentValue < lowerBound)
+        {
+            //Dashboard should show red and error, then make adjustment.
+            newValue = currentValue + tolerance/2;
+        }
+
+        //Set new value on sensor.
         sensor.setValue(newValue);
     }
     
--- AtmosForge-ECU/src/main/java/com/atmosforge/ecu/core/ECU.java
+++ AtmosForge-ECU/src/main/java/com/atmosforge/ecu/core/ECU.java
@@ -1,5 +1,8 @@
 package com.atmosforge.ecu.core;
 
+import com.atmosforge.ecu.abstracts.Actuator;
+import com.atmosforge.ecu.abstracts.Controller;
+import com.atmosforge.ecu.abstracts.Sensor;
 import com.atmosforge.ecu.actuators.HumidityActuator;
 import com.atmosforge.ecu.actuators.PressureActuator;
 import com.atmosforge.ecu.actuators.TemperatureActuator;
@@ -25,6 +28,8 @@ public class ECU {
     private final PressureActuator pressureActuator;
     private final HumidityActuator humidityActuator;
 
+    private boolean ecuActive;
+
     public ECU() {
         this.temperatureSensor = new TemperatureSensor("Temperature Sensor 1", 21, 2);
         this.pressureSensor = new PressureSensor("Pressure Sensor 1", 75, 1.5);
@@ -44,14 +49,61 @@ public class ECU {
         System.out.println("Starting AtmosForge Environment Control Unit...");
         //Later: initialize ECU modules or JavaFX app here
         ECU ecu = new ECU();
+        
         //Set initial test values
         ecu.getTemperatureSensor().setValue(25);
         ecu.getPressureSensor().setValue(101);
         ecu.getHumiditySensor().setValue(60);
+
         DashboardControl.setEcu(ecu);
         DashboardControl.main(args);
+
+        while (ecu.ecuActive)
+        {
+        ecu.runSystem(ecu.getTemperatureSensor(), ecu.getTemperatureController(), ecu.getTemperatureActuator());
+        ecu.runSystem(ecu.getPressureSensor(), ecu.getPressureController(), ecu.getPressureActuator());
+        ecu.runSystem(ecu.getHumiditySensor(), ecu.geHumidityController(), ecu.getHumidityActuator());
+        }
+
     }
- 
+
+    public void activateECU()
+    {
+        ecuActive = true;
+        
+        activateSystem(getTemperatureSensor(), getTemperatureController(), getTemperatureActuator());
+        activateSystem(getPressureSensor(), getPressureController(), getPressureActuator());
+        activateSystem(getHumiditySensor(), geHumidityController(), getHumidityActuator());
+    }
+
+    public void deactivateECU()
+    {
+        deactivateSystem(temperatureSensor, temperatureController, temperatureActuator);
+        deactivateSystem(pressureSensor, pressureController, pressureActuator);
+        deactivateSystem(humiditySensor, humidityController, humidityActuator);
+
+        ecuActive = false;
+    }
+
+    public void runSystem(Sensor sensor, Controller controller, Actuator actuator)
+    {
+        controller.monitor(actuator, sensor);
+    }
+
+    public void activateSystem(Sensor sensor, Controller controller, Actuator actuator)
+    {
+        sensor.activateSensor();
+        controller.activateController();
+        actuator.activateActuator();
+    }
+
+    public void deactivateSystem(Sensor sensor, Controller controller, Actuator actuator)
+    {
+        sensor.deactivateSensor();
+        controller.deactivateController();
+        actuator.deactivateActuator();
+    }
+
     public TemperatureSensor getTemperatureSensor() {
         return temperatureSensor;
     }
